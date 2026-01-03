c162fe8c15d2c8dbc23cbc7aca0c2f70227b35b8
yuniverse96 <easy9607@gmail.com>
2026-01-03 11:56:41 +0900

    #1차 마크업

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,57 @@
+import lpImg from '../../public/images/lp.png';
 
 function Home (){
     return(
         <>
             <div id="main">
-                <div className="left">
+                <section className="left">
                     <h2>my playlist</h2>
                     <div className="total_info">
                         <span>now</span><p>0</p>
                     </div>
-                    <div className="list_box">
+                    <div className="list_wrap">
                         <ul>
-                            <li>예시 예시</li>
+                            <li className="list_box">
+                                <div className="album_img"></div>
+                                <div className="album_info">
+                                    <h3>노래제목</h3>
+                                    <p>가수</p>
+                                </div>
+                            </li>
+                            <li className="list_box">
+                                <div className="album_img"></div>
+                                <div className="album_info">
+                                    <h3>노래제목</h3>
+                                    <p>가수</p>
+                                </div>
+                            </li>
+                            <li className="list_box">
+                                <div className="album_img"></div>
+                                <div className="album_info">
+                                    <h3>노래제목</h3>
+                                    <p>가수</p>
+                                </div>
+                            </li>
+                            <li className="list_box">
+                                <div className="album_img"></div>
+                                <div className="album_info">
+                                    <h3>노래제목</h3>
+                                    <p>가수</p>
+                                </div>
+                            </li>
                         </ul>
                     </div>
-
-
-
-                </div>
-                <div className="right"></div>
+                    <div className="bottom_btn">
+                        <button type="button">add music</button>
+                        <button type="button">save list</button>
+                    </div>
+                </section>
+                <section className="right">
+                    <div className="lp_wrap">
+                        <div className="vinyl"><img src={lpImg} alt="lp" /></div>
+                        <div className="cover_img"></div>
+                    </div>
+                </section>
             </div>
         </>
     )
